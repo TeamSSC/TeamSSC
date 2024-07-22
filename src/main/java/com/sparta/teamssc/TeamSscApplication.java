b6a28fe621ba4b87b99740a1ca04e7d5ab28f88f
dami0806 <dami0804@naver.com>
2024-07-22 10:27:05 +0900

    init setting

---
+++ src/main/java/com/sparta/teamssc/TeamSscApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.teamssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamSscApplication.class, args);
+    }
+
+}
