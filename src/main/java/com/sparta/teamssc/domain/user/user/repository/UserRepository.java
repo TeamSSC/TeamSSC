f847be33f7483c3ea0de0a00e42fce7ded3033b0
hyunmang2 <wowow19811@gmail.com>
2024-07-25 11:44:13 +0900

    feat[SSC-60] : findByStatus 추가

--- src/main/java/com/sparta/teamssc/domain/user/user/repository/UserRepository.java
+++ src/main/java/com/sparta/teamssc/domain/user/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.teamssc.domain.user.user.repository;
 
 import com.sparta.teamssc.domain.user.user.entity.User;
+import com.sparta.teamssc.domain.user.user.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findById(Long userId);
 
+    List<User> findByStatus(UserStatus userStatus);
 }
