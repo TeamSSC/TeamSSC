1d4e1d3decd8f53657600c854c02b73d63a9656a
kcm0411 <kcm0411@gmail.com>
2024-07-25 19:51:48 +0900

    feat[SSC-52]: 트랙 수정 기능 구현

--- src/main/java/com/sparta/teamssc/domain/track/controller/TrackController.java
+++ src/main/java/com/sparta/teamssc/domain/track/controller/TrackController.java
@@ -9,9 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tracks")
@@ -29,17 +27,31 @@ public class TrackController {
      * @return : 생성된 트랙 정보 (트랙명)
      */
     @PostMapping
-    public ResponseEntity<ResponseDto<TrackResponseDto>> createTrack(@AuthenticationPrincipal UserDetails userDetails, TrackRequestDto trackRequestDto){
+    public ResponseEntity<ResponseDto<TrackResponseDto>> createTrack(@RequestBody TrackRequestDto trackRequestDto) {
 
-        TrackResponseDto trackResponseDto = trackService.createTrack(userDetails.getUsername(), trackRequestDto);
+        TrackResponseDto trackResponseDto = trackService.createTrack(trackRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.<TrackResponseDto>builder()
                         .message("트랙을 생성했습니다.")
                         .data(trackResponseDto)
                         .build());
-
     }
 
+    /**
+     * 트랙 수정 기능 (관리자 권한 필요)
+     * @param : 수정할 트랙 id
+     * @return : 수정된 트랙 정보 (트랙명)
+     */
+    @PutMapping("/{trackId}")
+    public ResponseEntity<ResponseDto<TrackResponseDto>> updateTrack(@PathVariable Long trackId, @RequestBody TrackRequestDto trackRequestDto) {
 
+        TrackResponseDto trackResponseDto = trackService.updateTrack(trackId, trackRequestDto);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResponseDto.<TrackResponseDto>builder()
+                        .message("트랙이름을 수정했습니다.")
+                        .data(trackResponseDto)
+                        .build());
+    }
 }
--- src/main/java/com/sparta/teamssc/domain/track/service/TrackService.java
+++ src/main/java/com/sparta/teamssc/domain/track/service/TrackService.java
@@ -5,6 +5,7 @@ import com.sparta.teamssc.domain.track.dto.TrackResponseDto;
 
 public interface TrackService {
 
-    TrackResponseDto createTrack(String username, TrackRequestDto trackRequestDto);
+    TrackResponseDto createTrack(TrackRequestDto trackRequestDto);
 
+    TrackResponseDto updateTrack(Long trackId, TrackRequestDto trackRequestDto);
 }
