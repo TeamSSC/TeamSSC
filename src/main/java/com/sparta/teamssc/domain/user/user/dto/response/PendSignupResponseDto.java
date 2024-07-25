51cc717988a6ad34c4120b97f0889744177bcb1e
hyunmang2 <wowow19811@gmail.com>
2024-07-25 14:23:48 +0900

    fix[SSC-60] : Builder 어노테이션 추가, 필드 final 지정

--- src/main/java/com/sparta/teamssc/domain/user/user/dto/response/PendSignupResponseDto.java
+++ src/main/java/com/sparta/teamssc/domain/user/user/dto/response/PendSignupResponseDto.java
@@ -1,16 +1,18 @@
 package com.sparta.teamssc.domain.user.user.dto.response;
 
 import com.sparta.teamssc.domain.user.user.entity.UserStatus;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class PendSignupResponseDto {
 
-    private Long userId;
-    private String email;
-    private String username;
-    private UserStatus status;
+    private final Long userId;
+    private final String email;
+    private final String username;
+    private final UserStatus status;
 
+    @Builder
     public PendSignupResponseDto(Long userId, String email, String username, UserStatus status) {
         this.userId = userId;
         this.email = email;
