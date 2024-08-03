abcdd09f9a12e1a1ff9bad1a0e35392f28b57e23
Ham-Seung-Wan <syham001@naver.com>
2024-08-03 22:16:50 +0900

    chore [SSC] : 테스트로 인해 잠시 cors 모두 개방1

--- src/main/java/com/sparta/teamssc/common/config/WebMvcConfig.java
+++ src/main/java/com/sparta/teamssc/common/config/WebMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
