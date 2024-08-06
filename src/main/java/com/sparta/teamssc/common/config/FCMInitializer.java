badd048386c13c48f142b9e5b07c522d85508cf4
pk_hyun <pkh954533@gmail.com>
2024-08-06 20:09:37 +0900

    fix [SSC]: json 파일 secret에 직접 저장

--- src/main/java/com/sparta/teamssc/common/config/FCMInitializer.java
+++ src/main/java/com/sparta/teamssc/common/config/FCMInitializer.java
@@ -17,68 +17,17 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class FCMInitializer {
 
-    @Value("${fcm.firebase_project_id}")
-    private String projectId;
-
-    @Value("${fcm.firebase_private_key_id}")
-    private String privateKeyId;
-
-    @Value("${fcm.firebase_private_key}")
-    private String privateKey;
-
-    @Value("${fcm.firebase_client_email}")
-    private String clientEmail;
-
-    @Value("${fcm.firebase_client_id}")
-    private String clientId;
-
-    @Value("${fcm.firebase_auth_uri}")
-    private String authUri;
-
-    @Value("${fcm.firebase_token_uri}")
-    private String tokenUri;
-
-    @Value("${fcm.firebase_auth_provider_cert_url}")
-    private String authProviderCertUrl;
-
-    @Value("${fcm.firebase_client_cert_url}")
-    private String clientCertUrl;
-
-    @Value("${fcm.firebase_universe_domain}")
-    private String universeDomain;
+    @Value("${FIREBASE_JSON}")
+    private String firebaseJson;
 
     @PostConstruct
     public void initialize() {
         log.info("FCMInitializer 초기화");
         try {
-            log.info("Loading Firebase service account key from environment variables");
-
-            // 줄 바꿈 문자를 적절히 처리
-            String privateKey = System.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n");
-
-            // 환경 변수를 JSON 형식으로 변환
-            String json = String.format(
-                    "{"
-                            + "\"type\": \"service_account\","
-                            + "\"project_id\": \"%s\","
-                            + "\"private_key_id\": \"%s\","
-                            + "\"private_key\": \"%s\","
-                            + "\"client_email\": \"%s\","
-                            + "\"client_id\": \"%s\","
-                            + "\"auth_uri\": \"%s\","
-                            + "\"token_uri\": \"%s\","
-                            + "\"auth_provider_x509_cert_url\": \"%s\","
-                            + "\"client_x509_cert_url\": \"%s\","
-                            + "\"universe_domain\": \"%s\""
-                            + "}",
-                    projectId, privateKeyId, privateKey, clientEmail, clientId, authUri, tokenUri,
-                    authProviderCertUrl, clientCertUrl, universeDomain
-            );
-
-            // JSON 문자열을 InputStream으로 변환
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
+            log.info("Loading Firebase service account key from path: {}", firebaseJson);
 
             // Firebase 서비스 계정 키 파일로드
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(firebaseJson.getBytes(StandardCharsets.UTF_8));
             GoogleCredentials googleCredentials = GoogleCredentials.fromStream(inputStream);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(googleCredentials)
