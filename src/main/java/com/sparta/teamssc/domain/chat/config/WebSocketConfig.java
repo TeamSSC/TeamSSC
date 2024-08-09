fb0d173a9d3717e74be2c920da9d56bbe1054037
dami0806 <dami0804@naver.com>
2024-08-09 13:38:39 +0900

    fix: host name teamssc로 수정

--- src/main/java/com/sparta/teamssc/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/sparta/teamssc/domain/chat/config/WebSocketConfig.java
@@ -30,7 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         log.debug("STOMP Broker Relay 설정 중");
         registry.setPathMatcher(new AntPathMatcher("/"));
         registry.enableStompBrokerRelay("/topic", "/queue", "/exchange", "/amq/queue","/chat")
-                .setRelayHost("team-ssc.vercel.app")//("localhost")
+                .setRelayHost("teamssc.site")//("localhost")
                 .setRelayPort(15674)
                 .setClientLogin("guest")
                 .setClientPasscode("guest")
