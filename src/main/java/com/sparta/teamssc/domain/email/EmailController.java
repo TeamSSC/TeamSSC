53be398750fa70cea98559db9b6193781b12af89
pk_hyun <pkh954533@gmail.com>
2024-08-12 19:16:50 +0900

    fix [SSC-86]: 이메일 필요없는 부분 삭제

--- src/main/java/com/sparta/teamssc/domain/email/EmailController.java
+++ src/main/java/com/sparta/teamssc/domain/email/EmailController.java
@@ -1,11 +1,11 @@
 package com.sparta.teamssc.domain.email;
 
-import com.sparta.teamssc.domain.email.EmailService;
-import com.sparta.teamssc.domain.user.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public class EmailController {
 
     private final EmailService emailService;
-    private final UserService userService;
 
     /**
      * 이메일 발송 메서드
@@ -29,21 +28,5 @@ public class EmailController {
             return ResponseEntity.status(500).body("이메일 전송에 실패했습니다.");
         }
     }
-
-//    /**
-//     * 이메일 인증
-//     * @param username 사용자 이름
-//     * @param verificationCode 인증 코드
-//     * @return 인증 성공 또는 실패 메시지
-//     */
-//    @PostMapping("/verify-email")
-//    public ResponseEntity<String> verifyEmail(@RequestParam String username, @RequestParam String verificationCode) {
-//        boolean isVerified = userService.verifyEmail(username, verificationCode);
-//        if (isVerified) {
-//            return ResponseEntity.ok("이메일 인증 성공");
-//        } else {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이메일 인증 실패");
-//        }
-//    }
 }
 
