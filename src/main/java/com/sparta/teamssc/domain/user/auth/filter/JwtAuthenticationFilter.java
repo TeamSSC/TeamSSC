a6722cabefadd4cca95d72c6650d3cc052c3d51b
dami0806 <dami0804@naver.com>
2024-08-14 12:13:28 +0900

    fix: log추가 및, 로직 수정

--- src/main/java/com/sparta/teamssc/domain/user/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/teamssc/domain/user/auth/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,6 +27,7 @@ import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final UserDetailsService userDetailsService;
@@ -55,6 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             username = JwtUtil.getUsernameFromToken(jwt);
         }
 
+        try{
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
@@ -66,29 +69,46 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                log.info("엑세스 토큰유효 ");
 
             } else if (JwtUtil.isTokenExpired(jwt)) {
+                
                 // 엑세스 토큰이 만료된 경우 Json, Java 변환
                 ObjectMapper mapper = new ObjectMapper();
-                Map<String, String> requestBody = mapper.readValue(request.getInputStream(), Map.class);
-                String refreshToken = requestBody.get("refreshToken");
+                try {
+                    Map<String, String> requestBody = mapper.readValue(request.getInputStream(), Map.class);
 
-                if (refreshToken != null && JwtUtil.validateRefreshToken(refreshToken)) {
+                    String refreshToken = requestBody.get("refreshToken");
 
-                    // 리프레시 토큰이 유효한 경우 새로운 토큰 발급
-                    LoginResponseDto newTokens = JwtUtil.refreshAccessToken(requestBody, userRepository);
+                    if (refreshToken != null && JwtUtil.validateRefreshToken(refreshToken)) {
+                        // 리프레시 토큰이 유효한 경우 새로운 토큰 발급
+                        LoginResponseDto newTokens = JwtUtil.refreshAccessToken(requestBody, userRepository);
 
-                    response.setContentType("application/json");
-                    response.getWriter().write(mapper.writeValueAsString(newTokens));
-                } else {
-                    // 리프레시 토큰도 만료된 경우 401 에러 반환
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 만료되었습니다. 재로그인이 필요합니다.");
+                        response.setContentType("application/json");
+                        response.getWriter().write(mapper.writeValueAsString(newTokens));
+                        log.info("리프레시 토큰유효 , 엑세스 토큰 재발급");
+
+                        return; // 새 토큰을 발급했으므로 필터 체인을 중단, 응답 반환
+
+                    } else {
+                        // 리프레시 토큰도 만료된 경우 401 에러 반환
+                        log.info("리프레시 토큰도 만료");
+                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 만료되었습니다. 재로그인이 필요합니다.");
+                        return;
+                    }
+                } catch (IOException e) {
+                    log.warn("서버 오류가 발생했습니다. {}", e.getMessage());
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "요청 처리 중 오류가 발생했습니다.");
                     return;
-
                 }
             }
         }
         // 다음 필터로 요청을 전달
         chain.doFilter(request, response);
+        } catch (Exception e) {
+
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
+            log.warn("서버 오류가 발생했습니다. {}", e.getMessage());
+        }
     }
 }
